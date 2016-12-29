5a5c7a9688beb6b3822a958c40b63ff1f49ad343
linh1987 <linh1987@gmail.com>
2016-12-29 11:20:31 +0700

    display weather temp

--- js/weather.js
+++ js/weather.js
@@ -2,8 +2,8 @@
 var e = Inferno.createElement;
 
 var title = function (prop) {
-    return e('div', { className: "x_panel" },
-        e('div', { className: 'x_title' },
+    return div({ className: "x_panel" },
+        div({ className: 'x_title' },
             [
                 e('h2', {}, 'Daily active users',
                     e('small', {}, 'Sessions')
@@ -35,22 +35,94 @@ var title = function (prop) {
                         ),
                     ]
                 ),
-                e('div', { className: 'clearfix' })
+                div({ className: 'clearfix' })
             ]
         )
     );
 };
 
-var body = function(prop) {
-    
+var timeComponent = function (prop) {
+    return div({ className: 'col-sm-12' }, [
+        div({ className: 'temperature' }, [
+            e('b', {}, time && time.getDay && time.getDay()),
+            ", " + (time && time.toString && time.toString())
+        ]),
+    ]);
+}
+
+var getWeatherId = function (weather) {
+    if (weather === 'clear sky')
+        return 'clear-day';
+    return weather.replace(' ', '-');
+}
+
+var weatherIconComponent = function (prop) {
+    return div({ className: 'col-sm-4' }, [
+        div({ className: 'weather-icon' }, [
+            e('canvas', { height: 84, width: 84, id: getWeatherId(data.weather[0].description) })
+        ])
+    ]);
+}
+
+var weatherText = function (prop) {
+    return div({ className: 'col-sm-8' }, [
+        div({ className: 'weather-text' }, [
+            h2({}, [
+                data.name,
+                e('br'),
+                e('i', {}, (data.weather[0].description))
+            ])
+        ])
+    ]);
+}
+
+function convertFromKtoC(kTemp) {
+    return kTemp - 273.15;
+}
+
+var div = function (prop, children) {
+    return e('div', prop, children);
+}
+
+var h2 = function (prop, children) {
+    return e('h2', prop, children);
+}
+
+var h3 = function (prop, children) {
+    return e('h3', prop, children);
+}
+
+var weatherDegree = function (prop) {
+    return div({className: 'weather-text pull-right'}, [
+        h3({className: "degrees"}, 
+            convertFromKtoC(data.main.temp)
+        )
+    ]);
+}
+
+var body = function (prop) {
+    return e('div', { className: 'x_content' }, [
+        div({ className: "row" }, [
+            e(timeComponent),
+        ]),
+        div({ className: "row" }, [
+            e(weatherIconComponent),
+            e(weatherText)
+        ]),
+        div({ className: "row" }, [
+            e(weatherDegree),
+        ]),
+    ]
+    );
 }
 
 var data = '';
+var time = {};
 var weatherWidget = function (prop) {
-    return e('div', { className: 'col-md-6 col-sm-6 col-xs-12' },
+    return div({ className: 'col-md-6 col-sm-6 col-xs-12' },
         [
             e(title),
-            e('div', {}, data.weather[0].description)
+            e(body),
         ]
     );
 };
@@ -61,10 +133,12 @@ var render = function () {
         document.getElementById("weather-widget"));
 };
 
-fetch('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=41b32fae514addfcf8e801412ae4c88c')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=41b32fae514addfcf8e801412ae4c88c')
     .then(function (response) {
         return response.json();
     }).then(function (response) {
         data = response;
+        time = new Date();
         render();
+        renderSkycons();
     });
