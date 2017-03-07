da50a886d80d6e71d78e80c30584c150e3f4ad7d
ThanhNT <thanh.nt@niteco.se>
2017-03-07 13:13:22 +0700

    Build dev/production, uglify, clean, copy files

---
+++ _webpack.config.js
@@ -0,0 +1,44 @@
+
+var path = require('path');
+var webpack = require('webpack');
+var ReplaceHashWebpackPlugin = require('replace-hash-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+  	'main': './weather/app.js',
+    'vendor': ['inferno', 'redux', 'redux-thunk']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader"
+      }
+    ],
+  },
+
+  devtool: 'source-map',
+
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor' // Specify the common bundle's name.
+            }),
+    new HtmlWebpackPlugin({
+    	template: 'index.html',
+    	inject: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        sourceMap: true
+    })
+  ]
+};
---
+++ webpack.base.js
@@ -0,0 +1,38 @@
+
+var path = require('path');
+var webpack = require('webpack');
+var ReplaceHashWebpackPlugin = require('replace-hash-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'main': './weather/app.js',
+    'vendor': ['inferno', 'redux', 'redux-thunk']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader"
+      }
+    ],
+  },
+
+  devtool: 'source-map',
+
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor' // Specify the common bundle's name.
+            }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: true
+    })
+  ]
+};
