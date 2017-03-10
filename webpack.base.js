b8b03bbd02dfd59821e2f653d9df0a81119bb78b
Tai Nguyen <nguyentai.fit@gmail.com>
2017-03-10 15:24:16 +0700

    Revert "add less-loader, extract-text-plugin, file-loader"
    This reverts commit c5ed1cb1bf75961c04cf0a26acfb35ff1cb783b1.

--- webpack.base.js
+++ webpack.base.js
@@ -3,7 +3,6 @@ var path = require('path');
 var webpack = require('webpack');
 var ReplaceHashWebpackPlugin = require('replace-hash-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -17,32 +16,17 @@ module.exports = {
         test: /\.js?$/,
         loader: "babel-loader"
       },
-      // {
-      //   test: /\.css$/,
-      //   use: [
-      //     { loader: "style-loader" },
-      //     { loader: "css-loader" },
-      //   ],
-      // },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         loader: 'file-loader?name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.less$/,
-        loader: ['style-loader', 'css-loader', 'less-loader']
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'file-loader?name=images-phuc/[name].[hash].[ext]'
-      },
+      }
     ],
   },
 
@@ -54,7 +38,6 @@ module.exports = {
   },
 
   plugins: [
-    new ExtractTextPlugin("styles.css"),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor' // Specify the common bundle's name.
     }),
